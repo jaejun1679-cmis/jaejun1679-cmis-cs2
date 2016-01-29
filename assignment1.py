151a253918dfc11652ac88b0aabb7e7b0b66b162
cmis <cmis@cmis-hs05.(none)>
2016-01-29 10:55:44 +0700

    Started this hw

---
+++ assignment1.py
@@ -0,0 +1,26 @@
+myname = "JJ"
+myage = float(16.10)
+myheightmete = float(1.79)
+onesidesquare = 5
+lengthrect = 3
+heightrect = 2
+
+ageinmon = myage*12
+
+lefttolive = 80-myage
+
+myheightfeet = (myheightmete*3.28084)
+
+diffofheight = myheightmete-float(170.7)
+
+areaofsquare = onesidesquare**onesidesquare
+
+
+
+
+
+
+
+
+
+
