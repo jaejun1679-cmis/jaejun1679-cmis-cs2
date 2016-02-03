e45c1c57fd2b8c68a1098a2d2d2935b9e4d3be50
cmis <cmis@cmis-hs05.(none)>
2016-02-03 08:39:08 +0700

    Worked of assignment1

--- assignment1.py
+++ assignment1.py
@@ -1,26 +1,15 @@
 myname = "JJ"
-myage = float(16.10)
-myheightmete = float(1.79)
+myage = 16.10
+myheightmete = 1.79
 onesidesquare = 5
 lengthrect = 3
 heightrect = 2
-
 ageinmon = myage*12
-
 lefttolive = 80-myage
-
 myheightfeet = (myheightmete*3.28084)
-
-diffofheight = myheightmete-float(170.7)
-
+diffofheight = myheightmete-170.7
 areaofsquare = onesidesquare**onesidesquare
-
-
-
-
-
-
-
-
-
-
+halfofacube = (onesidesquare*onesidesquare*onesidesquare)/2
+ninthofrect = (lengthrect*heightrect)/9
+print "Hi. My name is " +myname+ ". " "I am currently " +str(myheightmete)+ " centimeters tall and in feet that will be " +str(myheightfeet)+ " feet. I am " + str(myage)+" years old and I have " + str(lefttolive) + " years left to live."
+print "Today I have one side of a square which is", onesidesquare,  "centimeters. Using that data, I found the area of the square it makes which is", areaofsquare, ". Seems like magic. Now I can find the half of the volume of the cube which is", halfofacube, ". I also have some values for a rectangle. Its length side is", lengthrect, "and the height is",heightrect  ". One ninth of area of this rectangle is",ninthofrect, "."
