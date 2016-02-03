d3ad53bc676ec888525aedf0041692b4d6f7392b
cmis <cmis@cmis-hs00.(none)>
2016-02-03 14:38:05 +0700

    Almost done with the assignment

--- assignment1.py
+++ assignment1.py
@@ -1,9 +1,9 @@
-myname = "JJ"
+myname = "JJ" #add comment here
 myage = 16.10
 myheightmete = 1.79
 onesidesquare = 5
-lengthrect = 3
-heightrect = 2
+lengthrect = 40
+heightrect = 12
 ageinmon = myage*12
 lefttolive = 80-myage
 myheightfeet = (myheightmete*3.28084)
@@ -12,4 +12,6 @@ areaofsquare = onesidesquare**onesidesquare
 halfofacube = (onesidesquare*onesidesquare*onesidesquare)/2
 ninthofrect = (lengthrect*heightrect)/9
 print "Hi. My name is " +myname+ ". " "I am currently " +str(myheightmete)+ " centimeters tall and in feet that will be " +str(myheightfeet)+ " feet. I am " + str(myage)+" years old and I have " + str(lefttolive) + " years left to live."
-print "Today I have one side of a square which is", onesidesquare,  "centimeters. Using that data, I found the area of the square it makes which is", areaofsquare, ". Seems like magic. Now I can find the half of the volume of the cube which is", halfofacube, ". I also have some values for a rectangle. Its length side is", lengthrect, "and the height is",heightrect  ". One ninth of area of this rectangle is",ninthofrect, "."
+print "Today I have one side of a square which is", onesidesquare, "centimeters. Using that data, I found the area of the square it makes which is", areaofsquare, ". Seems like magic. Now I can find the half of the volume of the cube which is", halfofacube, ". I also have some values for a rectangle. Its length side is", lengthrect, "and the height is",heightrect, ". One ninth of area of this rectangle is",ninthofrect, "."
+print (';)' *10000)
+
