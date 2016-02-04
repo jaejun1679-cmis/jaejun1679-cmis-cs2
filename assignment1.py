1408464a779c3be1ac6ad9995bfe8cfefd68877b
cmis <cmis@cmis-hs09.(none)>
2016-02-04 13:47:24 +0700

    Completed the first assignment

--- assignment1.py
+++ assignment1.py
@@ -1,17 +1,17 @@
-myname = "JJ" #add comment here
-myage = 16.10
-myheightmete = 1.79
-onesidesquare = 5
-lengthrect = 40
-heightrect = 12
-ageinmon = myage*12
-lefttolive = 80-myage
-myheightfeet = (myheightmete*3.28084)
-diffofheight = myheightmete-170.7
-areaofsquare = onesidesquare**onesidesquare
-halfofacube = (onesidesquare*onesidesquare*onesidesquare)/2
-ninthofrect = (lengthrect*heightrect)/9
-print "Hi. My name is " +myname+ ". " "I am currently " +str(myheightmete)+ " centimeters tall and in feet that will be " +str(myheightfeet)+ " feet. I am " + str(myage)+" years old and I have " + str(lefttolive) + " years left to live."
-print "Today I have one side of a square which is", onesidesquare, "centimeters. Using that data, I found the area of the square it makes which is", areaofsquare, ". Seems like magic. Now I can find the half of the volume of the cube which is", halfofacube, ". I also have some values for a rectangle. Its length side is", lengthrect, "and the height is",heightrect, ". One ninth of area of this rectangle is",ninthofrect, "."
-print (';)' *10000)
+myname = "JJ" #added value to a variable
+myage = 16.10 #added value to a variable
+myheightmete = 1.79 #added value to a variable
+onesidesquare = 5 #added value to a variable
+lengthrect = 40 #added value to a variable
+heightrect = 12 #added value to a variable
+ageinmon = myage*12 #executed simple mathematical function and stored the result in an appropriately named variable.
+lefttolive = 80-myage #executed simple mathematical function and stored the result in an appropriately named variable.
+myheightfeet = (myheightmete*3.28084) #executed simple mathematical function and stored the result in an appropriately named variable.
+diffofheight = myheightmete-170.7 #executed simple mathematical function and stored the result in an appropriately named variable.
+areaofsquare = onesidesquare**onesidesquare #executed simple mathematical function and stored the result in an appropriately named variable.
+halfofacube = (onesidesquare*onesidesquare*onesidesquare)/2 #executed simple mathematical function and stored the result in an appropriately named variable.
+ninthofrect = (lengthrect*heightrect)/9 #executed simple mathematical function and stored the result in an appropriately named variable.
+print "Hi. My name is " +myname+ ". " "I am currently " +str(myheightmete)+ " centimeters tall and in feet that will be " +str(myheightfeet)+ " feet. I am " + str(myage)+" years old and I have " + str(lefttolive) + " years left to live." #used the string function and linked variables to make a sentence
+print "Today I have one side of a square which is", onesidesquare, "centimeters. Using that data, I found the area of the square it makes which is", areaofsquare, ". Seems like magic. Now I can find the half of the volume of the cube which is", halfofacube, ". I also have some values for a rectangle. Its length side is", lengthrect, "and the height is",heightrect, ". One ninth of area of this rectangle is",ninthofrect, "." #used the string function and linked variables to make a sentence 
+print (';)' *10000) #printed out 10000 winky faces using the the string of ;) and the * function
 
