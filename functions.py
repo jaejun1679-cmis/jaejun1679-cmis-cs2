da15f01067760060b47cc294fc07d844be6b12fc
cmis <cmis@cmis-hs05.(none)>
2016-02-09 08:43:12 +0700

    Began assignment

---
+++ functions.py
@@ -0,0 +1,19 @@
+def add(a, b):
+	return (a+b)
+add(3, 4) 
+
+def sub(a, b):
+	return (a-b)
+sub(5, 3)
+
+def mul(a, b):
+	return (a*b)
+mul(4, 4)
+
+def div(a, b):
+	return (a/b)
+div(2, 3)
+
+def hours_from_seonds(a):
+    return (a/3600)
+hours_from_seconds(86400) 
