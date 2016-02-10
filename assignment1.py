22315e5b6606778cb407265f812207bbda3b5cd4
cmis <cmis@cmis-hs05.(none)>
2016-02-10 10:55:40 +0700

    Work on my assignment

--- assignment1.py
+++ assignment1.py
@@ -8,10 +8,9 @@ ageinmon = myage*12 #executed simple mathematical function and stored the result
 lefttolive = 80-myage #executed simple mathematical function and stored the result in an appropriately named variable.
 myheightfeet = (myheightmete*3.28084) #executed simple mathematical function and stored the result in an appropriately named variable.
 diffofheight = myheightmete-170.7 #executed simple mathematical function and stored the result in an appropriately named variable.
-areaofsquare = onesidesquare**onesidesquare #executed simple mathematical function and stored the result in an appropriately named variable.
+areaofsquare = onesidesquare*onesidesquare #executed simple mathematical function and stored the result in an appropriately named variable.
 halfofacube = (onesidesquare*onesidesquare*onesidesquare)/2 #executed simple mathematical function and stored the result in an appropriately named variable.
 ninthofrect = (lengthrect*heightrect)/9 #executed simple mathematical function and stored the result in an appropriately named variable.
 print "Hi. My name is " +myname+ ". " "I am currently " +str(myheightmete)+ " centimeters tall and in feet that will be " +str(myheightfeet)+ " feet. I am " + str(myage)+" years old and I have " + str(lefttolive) + " years left to live." #used the string function and linked variables to make a sentence
 print "Today I have one side of a square which is", onesidesquare, "centimeters. Using that data, I found the area of the square it makes which is", areaofsquare, ". Seems like magic. Now I can find the half of the volume of the cube which is", halfofacube, ". I also have some values for a rectangle. Its length side is", lengthrect, "and the height is",heightrect, ". One ninth of area of this rectangle is",ninthofrect, "." #used the string function and linked variables to make a sentence 
-print (';)' *10000) #printed out 10000 winky faces using the the string of ;) and the * function
-
+print (";)" *10000) #printed out 10000 winky faces using the the string of ;) and the * function
--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+import math
 def add(a, b):
 	return (a+b)
 add(3, 4) 
@@ -14,6 +15,25 @@ def div(a, b):
 	return (a/b)
 div(2, 3)
 
-def hours_from_seonds(a):
+def hours_from_seconds(a):
     return (a/3600)
-hours_from_seconds(86400) 
+hours_from_seconds(86400)
+
+def circle_area(a):
+	return (math.pi*(a**2))
+circle_area(5)
+
+def sphere_volume(a):
+    return (4*math.pi*(a**2))
+sphere_volume(5)
+
+def avy_volume(a, b):
+    a/2
+    b/2
+    return ((4*math.pi*(a**2)) + (4*math.pi*(b**2)))/2
+avy_volume(10 ,20)
+
+def area_tri(a, b, c):
+   h = math.sqrt((1**2) + (2**2))
+    return (2*0.5*h)
+
