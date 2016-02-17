66ed584d8e251d10199b0403ad129fbe64b3537a
cmis <cmis@cmis-hs05.(none)>
2016-02-17 13:27:50 +0700

    Just in case

--- simple.py
+++ simple.py
@@ -6,9 +6,9 @@ Your current BMI is: {} kg/m^2
 
 def main():
 	name = raw_input("What is your name?: ")
-	h1 = raw_input("What is you height? (meters only): ")
-	w1 = raw_input("What is your weight? (kilograms only): ")
-	h2 = float(h1)
+	h1 = raw_input("What is you height? (centimeters only): ")
+	w1 = raw_input("What is your weight? (kilograms only): ")	
+	h2 = float(h1) * 100
 	w2 = float(w1)
 	bmi = w2 / (h2**2)
 	print output(name, h2, w2, bmi)
