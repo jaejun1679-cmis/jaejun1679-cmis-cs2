c655a424df0bdf473ad2283a987bd4f652f226e9
cmis <cmis@cmis-hs05.(none)>
2016-02-24 10:01:20 +0700

    Caught up with all the assignments with no errors

--- functions.py
+++ functions.py
@@ -1,33 +1,43 @@
 import math #imported the module so that math functions can be executed 
+
+def msg_box(txt):
+	return "+" + ((len(txt)+4)*"-") + "+" + "\n" + "|" + (2*" ") + (txt)+ (2*" ") + "|" + "\n" + "+" + ((len(txt)+4)*"-") + "+"
+
 def add(a, b): #defined a function
 	return (a+b) #told what the value the function should return; hence the name
 added = add(3, 4) #called the function and stored the results in a variable 
-
+print  msg_box(str(added))
 
 def sub(a, b): 
 	return (a-b)
 subbed = sub(5, 3)
+print  msg_box(str(subbed))
 
 def mul(a, b):
 	return (a*b)
 multi = mul(4, 4)
+print  msg_box(str(multi))
 
 def div(a, b):
 	return (a/b)
 divid = div(2, 3)
+print  msg_box(str(divid))
 
 def hours_from_seconds(a):
     return (a/3600)
 hts = hours_from_seconds(86400)
+print  msg_box(str(hts))
 
 def circle_area(a):
 	return (math.pi*(a**2))
 cA = circle_area(5)
+print  msg_box(str(cA))
 
 def sphere_volume(a):
     b = float(4/3)
     return ((b)*math.pi*(a**3))
 sV = sphere_volume(5)
+print  msg_box(str(sV))
 
 def avg_volume(a, b):
     c = float(a/2)
@@ -35,17 +45,20 @@ def avg_volume(a, b):
     z = float(4/3)
     return (((z)*math.pi*(c**3)) + ((z)*math.pi*(d**3)))/2
 avgV = avg_volume(10 ,20)
+print  msg_box(str(avgV))
 
 def area_tri(a, b, c):	
     s = (a + b + c)/2
     return float(math.sqrt((s*(s-a)*(s-b)*(s-c))))
 triA = area_tri(1, 2, 2.5)
+print  msg_box(str(triA))
 
 def right_align(a):
-	print ((" "*80) - str(len((a)))
-rightal = right_align("Hello World!")
+	return (80 - len(a)) * " " + a
+rightaltxt = right_align("Hello World!")
+print  msg_box(str(rightaltxt))
 
-def msg_box(txt):
-	str (a)
-	return "+" + ((len(txt)+4)*"-") + "+" + "\n" + "|" + (2*" ") + (txt)+ (2*" ") + "|" + "\n" + "+" + ((len(txt)+4)*"-") + "+"
-msg_box = msg_box("I eat cats!")
+def center_txt(a):
+	return (40 - len(a)) * " " + a
+centertxt = center_txt("Hello World!")
+print  msg_box(str(centertxt))
