19633f145a4cea13899a986b555b0e0542515768
cmis <cmis@cmis-hs05.(none)>
2016-03-07 10:55:27 +0700

    Working on my own assignment

---
+++ dontcallthisrandomty.py
@@ -0,0 +1,40 @@
+def waterbend():
+    print "Water splash" 
+
+def firebend(): 
+    print "Fire bolt"
+
+def earthbend(): 
+    print "Rock throw" 
+
+def airbend():
+    print "Wind attack"
+
+"Yes" and "yes" = True
+
+"No" and "no"  = False
+
+
+def question(name, yon): 
+    if yon == True:
+        firebend()
+        earthbent()
+        airbend()
+        waterbend()
+    elif 
+
+raw_input = question(name
+        
+
+Hello {}. Are you an avartar?
+     if yon: 
+         
+
+
+def main():
+    name = raw_input("What is your name?: ")
+    yon = raw_input("Are you an Avatar?: ")
+
+
+
+
---
+++ hurhurhur.py
@@ -0,0 +1,14 @@
+import math
+import random
+import time
+
+while True:
+   
+    if x = "go" == "raw_input("Type "go": )
+ 
+    print x
+
+    time.sleep(5)
+
+a(5**5**5)
+
