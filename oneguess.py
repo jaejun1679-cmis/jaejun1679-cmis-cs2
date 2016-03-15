a1c500cc8cd9425db07d0fae862ad6cc150675b8
cmis <cmis@cmis-hs05.(none)>
2016-03-15 13:28:23 +0700

    Done with the guessing game

--- oneguess.py
+++ oneguess.py
@@ -1,11 +1,40 @@
+import math
+import random
 
 def output(minnum, maxnum):
 	return """
 I'm thinking from a number from {} to {}.
 """.format(minnum, maxnum)
 
+def result(target, guessnum, offby):
+
+    if target > guessnum:
+        print """
+The target was {}.
+Your guess was {}.
+That is over {} units.
+""".format(target, guessnum, offby)
+
+    elif target == guessnum:
+        print """
+The target was {}.
+Your guess was {}. 
+Therefore you are CORRECTIO! ARE U A WIZARD?
+""".format(target, guessnum, offby)
+
+    else:
+        print """
+The target was {}.
+Your guess was {}. 
+That is over {} units.
+""".format(target, guessnum, offby)
+
 def main():
-    minnum = raw_input("What is the minimum number?: ")
-    maxnum = raw_input("What is the maximum number?: ")
+    minnum = float(raw_input("What is the minimum number?: "))
+    maxnum = float(raw_input("What is the maximum number?: "))
     print output(minnum, maxnum)
+    guessnum = float(raw_input("What do you think it is?: "))
+    target = random.randint(minnum, maxnum)
+    offby = abs(target - guessnum)
+    result(target, guessnum, offby)
 main()
