efa7578f55188781c8930301fed3e77f24a4f191
cmis <cmis@cmis-hs14.(none)>
2016-03-22 13:12:31 +0700

    Made a looping function. Not complete almost done

--- conditionals.py
+++ conditionals.py
@@ -1,6 +1,7 @@
 #The game will give the user the illusion of choice. Similar to the game by the name "The Stanley Parable", there will be a narrator which I will code. Primarily using the raw_input function, it will ask the user to type something. If they obey, the story will continue. If they disobey, the story will alter. My program allow the user to control a character named "Bob". Bob is trying to go to work and it is the user's responsibility to make sure he showers, eats breakfast, wears proper clothing, and get the correct briefcase.  
 import random
 import math
+import time
 
 def intro():
 
@@ -13,7 +14,7 @@ Before you begin, take account this quote:
 Made no sense but thy shall not judge the ones who try. 
 """
 
-def scene1():
+def wakeup():
 
     answer1 = raw_input("Bob needs to go to work, but should he wake up? (yes or no) ")
     
@@ -26,9 +27,9 @@ def scene1():
 
     else: 
         print """It clearly says to type "yes" or "no" you retard...\n"""
-        exit()
+        wakeupscene()
 
-def scene2():
+def takeshower():
 
     answer2 = raw_input("Tadaaa now Bob woke up. But Bob is very bad smell. Should Bob take a shower? (yes or no) ")
     
@@ -40,40 +41,61 @@ def scene2():
         exit()
 
     else: 
-        print """Are you in the umost okay condition right now? Like I've mentioned, please type "yes" OR "no". Just two simple words.\n"""
-        exit()
+        print """Are you in the umost okay condition right now? Like I mentioned before, please type "yes" OR "no". Just two simple words bruv.\n"""
+        showerscene()
 
-def scene3():
+def eatbreakfast():
 
     answer3 = raw_input(" Eat breakfast? Now how about that. It really doesn't matter when you eat but eh. So...eat? (yes or no) ")
     
     if  answe3 == "yes":
-        print "NO. HE MUST NOT EAT AT HOME, for money management purposes. If he goes to his office, there must be a (insert the food corner in offce place) \n" #do this 
+        print "NO. HE MUST NOT EAT AT HOME, for money management purposes. If he goes to his office, there must be be free food. You know that place where there is a small fridge and it is like a lounge.  \n" #do this 
         exit()
     
     elif answer3 == "no":
-        print """ \n"""
+        print """Yas. For reasons along the line of "Be cheap in order to save money" he must not eat at home but eat the free food provided the office place he works at. Ohhh maybe he works for Google or something. idk lel \n"""
 
     else: 
-        print """ \n"""
+        print """ Okay now. I'm done with you incapacities to type "yes" or "no". You GPA must be below 2.5. bye. \n"""
         exit()
 
-def scene4():
+def wearsuit():
 
-    answer4 = raw_input(" (yes or no) ")
+    answer4 = raw_input("As all classy meny and bussiness workers, they must (men and women; I'm not sexist) wear suits (or maybe that's just some social schema one must conform to). At hand, YOU know Bob works in an office and he is getting ready for work. So now u tell me, SHOULD he wear a suit? Hint: The answer is yes. (yes or no) ")
     
     if  answe4 == "yes":
-        print " \n"
+        print " NICE. At least you know how business men dress to work. Now Bob is wearing a suit and tie~ \n"
     
     elif answer4 == "no":
-        print """ \n"""
-        exit()
+        print """...................okay im getting tired. lets finish this quick.\n"""
+        wearsuit()
 
     else: 
-        print """ \n"""
-        exit()
+        print """
+My German Youth, after a year's time, I can now greet you here again!
 
-def scene5():
+Since then, enormous events have taken place in Germany. Already twelve months ago, the battle for power has granted us success. In the meantime, our movement-of which you are the young guards today and future bearers-has taken hold of one position after another in our country and bestowed it to the German people. At the same time, your movement has grown from an already large union to the largest youth organization in the world. This is the result of numerous contributors whose leader I assigned to you, party comrade, Mr. Schirach.In this vast arena here today, you are a part of what people stand for in this country. You must now take on and learn in your youth what we want to see take shape in all of Germany. We know that nothing is handed to a people but everything must be fought for and conquered. There is nothing one can be a master of that one has not first learned and instilled into oneself. And so now, we want that you, German boys and German girls, take up all that we have hope for in you and our country-what we want to see fulfilled in Germany. We want to be ONE nation, and you my youth, you shall now become this nation. We do not want to see class and status differences anymore, and so you must not allow yourselves to nurture attitudes that promote them. We want to see ONE Reich, and you must already now train yourselves for this in ONE organization. We want our folk to be loyal, and you must learn this loyalty. We want our people to be obedient, and you must practice obedience. We want our folk to be peace loving, but also brave. You must always be ready for peace and at the same time courageous. We do not want our people to be weaklings but that they can be tough in order to withstand the difficulties of life. And you must already train yourselves for this in your youth. We want our people to love honor again, und you must declare yourselves to the principle of honor already in your youngest years. We want you to be a proud people again, and in your youth you must attain this pride; a proud people from which springs the pride of your youth and carries into the generations to come. All that we demand from the Germany of the future, we demand from you, boys and girls. This you must practice and in this contribute to the future. No matter what we create today and what we do, we will pass on one day. But in you Germany will continue. And when there will be nothing left of us, then you must bear the flag in your hand-a flag which we once raised from nothing. You must therefore stand confident on the soil of your country, and you must be strong so that this flag will never be taken from you. And then, when generation after generation comes after you, you will have the right to demand the same from them. Then you can demand from your future youth that they be like you were. And then Germany looks at you also with pride, and everyone's heart runs over with joy when we see you. And when we see in you the promise that our work is not in vain, but that it bears fruit in our country, then we are all gripped with a proud happiness to see in you the fulfillment of our work. With that we have the assurance that the Millions who died in WWI-the great numbers of our comrades-have not made the sacrifice for Germany in vain, so that in the end, out from all of it, a one-spirited, free, proud, and honor-loving people will emerge. And I know it cannot be any other way because you are flesh from our flesh and blood from our blood, and in your young minds burns the same spirit that dominates us. You cannot be any other way but be bound to us. And when the large processions of our movement march today gloriously through Germany, then we know that you will join these columns, and we know that before us lays Germany, in us marches Germany, and after us comes Germany!" - Hitler, Sept 8, 1934. 
+
+In short, Hitler is talking about the young mind of Germany. Now the fact that you cannot type "yes" or "no" is like...sigh nvm. From now on I will talk in some form on English you will understand. \n"""
+        altcarstart()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def altercarstart():
 
     answer5 = raw_input(" (yes or no) ")
     
@@ -88,11 +110,8 @@ def scene5():
         print """ \n"""
         exit()
 
+
 def main():
     intro()
-    scene1()
-    scene2()
-    scene3()
-    scene4()
-    scene5()
+    
 main()
--- dontcallthisrandomty.py
+++ dontcallthisrandomty.py
@@ -1,40 +1,19 @@
-def waterbend():
-    print "Water splash" 
+def wearsuit(trynum):
 
-def firebend(): 
-    print "Fire bolt"
+    answer4 = raw_input("As all classy meny and bussiness workers, they must (men and women; I'm not sexist) wear suits (or maybe that's just some social schema one must conform to). At hand, YOU know Bob works in an office and he is getting ready for work. So now u tell me, SHOULD he wear a suit? Hint: The answer is yes. (yes or no)\n")
 
-def earthbend(): 
-    print "Rock throw" 
+    if answer4 == "yes":
+        print "NICE. At least you know how business men dress to work. Now Bob is wearing a suit and tie~\n"
+    
+    elif answer4 == "no":
+        print """...................okay im getting tired. lets finish this quick. like i said, he must wear a suit to work.\n"""
+        trynum = trynum + 1
 
-def airbend():
-    print "Wind attack"
-
-"Yes" and "yes" = True
-
-"No" and "no"  = False
-
-
-def question(name, yon): 
-    if yon == True:
-        firebend()
-        earthbent()
-        airbend()
-        waterbend()
-    elif 
-
-raw_input = question(name
+    if trynum == 3:
+        print "yoyoyoyoyo"
         
-
-Hello {}. Are you an avartar?
-     if yon: 
-         
-
-
 def main():
-    name = raw_input("What is your name?: ")
-    yon = raw_input("Are you an Avatar?: ")
+	wearsuit(0)
+main()
 
-
-
-
+#place "wearsuit(trynum)" somewhere else so that the program can make sure trynum == 3 and print yoyoyo instead of calling the wearsuit function again
