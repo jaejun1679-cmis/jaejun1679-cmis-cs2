b694f4b3a775a1aa345398739b32b0cdc00dc36c
cmis <cmis@cmis-hs04.(none)>
2016-03-28 14:20:58 +0700

    Changed simple.py to fit the requirments

--- simple.py
+++ simple.py
@@ -4,13 +4,15 @@ Hello {}!
 Your current BMI is: {} kg/m^2
 """.format(name, bmi)
 
+def findbmi(w2, h2):
+    return w2 / (h2**2)
+
 def main():
 	name = raw_input("What is your name?: ")
 	h1 = raw_input("What is you height? (centimeters only): ")
 	w1 = raw_input("What is your weight? (kilograms only): ")	
 	h2 = float(h1) * 100
 	w2 = float(w1)
-	bmi = w2 / (h2**2)
+	bmi = findbmi(w2, h2)
 	print output(name, h2, w2, bmi)
-
 main()
