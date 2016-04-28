dc0fcc5c4ea83b83b009a3a0a184ccfdc6bec342
cmis <cmis@cmis-hs16.(none)>
2016-04-28 13:07:55 +0700

    Starttoend.py is now working

--- startotend.py
+++ startotend.py
@@ -1,30 +1,41 @@
 import time
 
-def find(start, end):
+def find(start, end, attempt):
     if start > end:
-        countdownfrom(start, end)
+        countdownfrom(start, end, attempt)
     elif end > start: 
-        countupfrom(start, end)
+        countupfrom(start, end, attempt)
 
-def countdownfrom(start, end):
+def countdownfrom(start, end, attempt):
+
     if start == end:
         print "We made it to " + str(end) + "!"
+
     else:
         time.sleep(1)
-        print start - 1 #fix tihs
-        countdownfrom((start + 1), end) #fix tihs
+        if attempt == 0:
+            print start
+        print start - 1
+        attempt = attempt + 1
+        countdownfrom((start - 1), end, attempt)
 
-def countupfrom(start, end):
+def countupfrom(start, end, attempt):
+
     if start == end:
         print "We made it to " + str(start) + "!"
+
     else:
         time.sleep(1)
-        print start + 1 #fix tihs
-        countdownfrom((start +1), end) #fix tihs
+        if attempt == 0:
+            print start
+        print start + 1
+        attempt = attempt + 1
+        countupfrom((start + 1), end, attempt)
 
 def main():
     start = float(raw_input("Insert the starting number: "))
     end = float(raw_input("Insert the ending number: "))
-    find(start, end)
+    attempt = 0
+    find(start, end, attempt)
 
 main()
