e36ada44c8dd8db41283d69cd30b1bb8a9fdfc6f
cmis <cmis@cmis-hs15.(none)>
2016-05-16 10:42:45 +0700

    Done with recurring oneguess game too

--- recursiononeguess.py
+++ recursiononeguess.py
@@ -2,10 +2,19 @@ import random
 
 def findnumber(minnum, maxnum, target):
 
-    guessnum = float(raw_input("What do you think it is?: "))
+    guessnum = float(raw_input("\nWhat do you think it is?: "))
 
     if target == guessnum:
         print "\nYou are CORRECT! ARE U A WIZARD?!"
+        exit()
+
+    elif guessnum > maxnum or guessnum < minnum:
+        print "\nYou have exceeded the limits set by yourself."
+        useranswer = raw_input("Would you like to try again? ")
+        if useranswer == "yes":
+            findnumber(minnum, maxnum, target)
+        else:
+            exit()
         exit()
 
     elif target > guessnum:
@@ -16,19 +25,14 @@ def findnumber(minnum, maxnum, target):
         print "\nThat's too high! Please try again."
         findnumber(minnum, maxnum, target)
 
-    elif guessnum > maxnum and guessnum < minnum:
-        print "\nYou have exceeded the limits set by yourself."
-        exit()
-
-
 def output(minnum, maxnum):
 	return """
 I am thinking from a number from {} to {}.
 """.format(minnum, maxnum)	    
 
 def main():
-    minnum = float(raw_input("What is the minimum number?: "))
     maxnum = float(raw_input("What is the maximum number?: "))
+    minnum = float(raw_input("What is the minimum number?: "))
     print output(minnum, maxnum)
     target = random.randint(minnum, maxnum)    
     findnumber(minnum, maxnum, target)
